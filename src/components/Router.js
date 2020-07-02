68e1f7aae6bb554ae2c6600145f218a28e3504cd
Jeremy Bissonnette <jeremy@kwmassage.com>
2020-07-02 08:03:40 -0400

    rounter in app not working

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import NotFound from "./NotFound";
 import App from "./App";
 import Src from "./Src";
 import Details from "./Details";
 
-const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/src" exact component={Src} />
-      <Route path="/src/ability-scores" exact component={Details} />
-      <Route path="/src/:category" component={Src} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
-);
+class Router extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Route path="/" exact component={App} />
+        <Route path="/src" exact component={Src} />
+        <Route path="/src/ability-scores" exact component={Details} />
+        <Route path="/src/:category" component={Src} />
+        <Route component={NotFound} />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Router;
